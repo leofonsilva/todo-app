a8765be522a7c69d041e0bfa8ab4cb87b5d51ce1
Leonardo Fonseca Silva <leonardofonsecasilva@hotmail.com>
2025-07-31 20:37:08 -0300

    Add initial setup scripts for Git configuration and workspace

---
+++ setup-git-perfil.bat
@@ -0,0 +1,25 @@
+@echo off
+SETLOCAL
+
+REM User settings
+SET USER_NAME=Leonardo Fonseca Silva
+SET USER_EMAIL=leonardofonsecasilva@hotmail.com
+
+REM Local Git configuration
+git config user.name "%USER_NAME%"
+git config user.email "%USER_EMAIL%"
+
+REM Display local configurations
+echo Local information:
+git config --local user.name
+git config --local user.email
+
+echo.
+
+REM Display global configurations
+echo Global information:
+git config --global user.name
+git config --global user.email
+
+ENDLOCAL
+pause
